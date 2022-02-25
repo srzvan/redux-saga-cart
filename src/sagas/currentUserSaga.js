3888d463973c0d272096d024e2248828e8508dd7
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-02-25 17:26:08 +0200

    constants file in /server

--- src/sagas/currentUserSaga.js
+++ src/sagas/currentUserSaga.js
@@ -1,12 +1,13 @@
 import fetch from 'isomorphic-fetch';
 import { take, put, call, apply } from 'redux-saga/effects';
 
+import { SERVER_BASE_URL } from '../../server/constants';
 import { GET_CURRENT_USER_INFO, setCurrentUser } from '../actions';
 
 export function* currentUserSaga() {
   const { id } = yield take(GET_CURRENT_USER_INFO);
 
-  const response = yield call(fetch, `http://localhost:8081/user/${id}`);
+  const response = yield call(fetch, `${SERVER_BASE_URL}/user/${id}`);
   const user = yield apply(response, response.json);
 
   yield put(setCurrentUser(user));
