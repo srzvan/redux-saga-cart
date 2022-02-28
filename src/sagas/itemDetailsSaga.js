f7ab760a71657d432c5b44f24b1b2fb1ab2901d0
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-02-28 14:10:24 +0200

    fix itemDetailsSaga

--- src/sagas/itemDetailsSaga.js
+++ src/sagas/itemDetailsSaga.js
@@ -1,5 +1,5 @@
 import fetch from 'isomorphic-fetch';
-import { take, fork, put, apply, call } from 'redux-saga/effects';
+import { take, fork, put, apply, call, all } from 'redux-saga/effects';
 
 import { SERVER_BASE_URL } from '../../server/constants';
 import { SET_CART_ITEMS, setItemDetails } from '../actions';
@@ -7,7 +7,7 @@ import { SET_CART_ITEMS, setItemDetails } from '../actions';
 export function* itemDetailsSaga() {
   const { items } = yield take(SET_CART_ITEMS);
 
-  yield items.map((item) => fork(loadItemDetails, item));
+  yield all(items.map((item) => fork(loadItemDetails, item)));
 }
 
 export function* loadItemDetails(item) {
@@ -15,7 +15,6 @@ export function* loadItemDetails(item) {
 
   const response = yield call(fetch, `${SERVER_BASE_URL}/items/${id}`);
   const data = yield apply(response, response.json);
-  console.log(data);
 
   yield put(setItemDetails(...data));
 }
